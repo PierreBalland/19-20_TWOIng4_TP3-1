b290d83b31ffd7dec0f789cc22f4f85760c057c6
Pierre <pierre.balland@edu.ece.fr>
2019-11-13 20:13:16 +0100

    start

---
+++ tp-react-pballand/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+    render() {
+        return (
+
+                <div id="square">
+
+                    <header>Prenom</header>
+                    <p> Nom </p>
+                    <p>Date de naissance </p>
+
+
+            </div>
+        );
+    }
+}
+export default App
