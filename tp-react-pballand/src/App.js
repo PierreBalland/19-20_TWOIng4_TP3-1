22f1d7de39888441a834bfe5f24a156df3fdeb5c
Pierre <pierre.balland@edu.ece.fr>
2019-11-14 23:06:42 +0100

    version avec images et un peu plus propre

--- tp-react-pballand/src/App.js
+++ tp-react-pballand/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import pam from './pam.jpg';
+import michael from './michael.jpg';
+import dwight from './dwight.jpg';
 import './App.css';
 
-const globale="michael";
-const DEFAULT_STATE = { name: 'Scott', frstname: 'Michael', date: '07/11/65' }; // Michael
-const DEFAULT_STATE1 = { name: 'Schrute', frstname: 'Dwight', date: '05/08/66' }; //Dwight
-const DEFAULT_STATE2 = { name: 'Beasley', frstname: 'Pam', date: '23/04/97' }; //Pam
+
+const DEFAULT_STATE = { name: 'Scott', frstname: 'Michael', date: '07/11/65' , picture:michael}; // Michael
+const DEFAULT_STATE1 = { name: 'Schrute', frstname: 'Dwight', date: '05/08/66',picture:dwight }; //Dwight
+const DEFAULT_STATE2 = { name: 'Beesly', frstname: 'Pam', date: '23/04/97', picture:pam }; //Pam
 
 class Carreprincipale extends React.Component { //le carre principale a un prenom un nom date et photo a afficher
 constructor(prenom,nom,date,pic){
@@ -38,12 +41,17 @@ constructor(prenom,nom,date,pic){
 const nom=this.state.name;
 const prenom=this.state.frstname;
 const date=this.state.date;
+        const picture=this.state.picture;
         return (
                 <div id="glob">
-                    <button className="favorite styled"type="button" onClick={() => this.handleClick()}>Michael</button> <button className="favorite styled"type="button" onClick={() => this.handleClick1()}>Dwight</button>
-                    <button className="favorite styled"type="button" onClick={() => this.handleClick3()}>Pam</button>
+                    <div id="buttons">
+                    <button className="favorite styled"type="button" onClick={() => this.handleClick()}>Michael</button> &nbsp; &nbsp; &nbsp;<button className="favorite styled"type="button" onClick={() => this.handleClick1()}>Dwight</button>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button className="favorite styled"type="button" onClick={() => this.handleClick3()}>Pamela</button></div>
                     <div id="square">
-                        {nom}
+
+
+                        <img src={picture}/>
+                        <p> {nom} </p>
 
                     <p> {prenom} </p>
                     <p>{date} </p>
@@ -58,7 +66,7 @@ const date=this.state.date;
 
 
 function f() {
-    globale="andre";
+
     window.location.reload();
 }
 
