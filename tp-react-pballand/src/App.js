bcc59ee9f713bc8b64d99c4c3a85c6faf9a584ee
Pierre <pierre.balland@edu.ece.fr>
2019-11-14 18:58:02 +0100

    updates

--- tp-react-pballand/src/App.js
+++ tp-react-pballand/src/App.js
@@ -2,19 +2,73 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
+class Carreprincipale extends React.Component { //le carre principale a un prenom un nom date et photo a afficher
+constructor(prenom,nom,date,pic){
+    super();
+    this.prenom=prenom;
+    this.nom=nom;
+    this.date=date;
+    this.pic=pic;
+}
     render() {
+
+const test =this.props.pic;
         return (
-
                 <div id="square">
+                    {this.props.pic}
+                    <img className="fit-picture" src={test}/>
+                    {this.props.prenom}
 
-                    <header>Prenom</header>
-                    <p> Nom </p>
-                    <p>Date de naissance </p>
+                    <p> {this.props.nom} </p>
+                    <p>{this.props.date} </p>
 
 
             </div>
         );
     }
+
 }
-export default App
+class Topbuttons extends React.Component { //le carre principale a un prenom un nom date et photo a afficher
+    constructor(){
+        super();
+        }
+    render() {
+
+
+        return (
+            <div>
+                <button className="favorite styled"
+                        type="button">
+                    Add to favorites
+                </button>
+
+
+            </div>
+        );
+    }
+
+}
+class Personne  { //le carre principale a un prenom un nom date et photo
+    constructor(prenom,nom,date,pic){
+
+        this.prenom=prenom;
+        this.nom=nom;
+        this.date=date;
+        this.pic=pic;
+    }
+
+
+}
+class Page extends React.Component {
+    render() {
+        return (
+            <div>
+            <Topbuttons/>
+            <Carreprincipale prenom={"Michael"} nom={"Scott"} date={"07/11/65"} pic={"michael.jpg"}/>
+            </div>
+        );
+    }
+}
+
+// ========================================
+export default Page
